b2ebcf82b2c969fedbe6f6b3d2f5402972f13331
Ertugrul Karademir <ertugrul.karademir@oliverwyman.com>
2018-08-21 21:31:42 +0100

    Starting on audio api

--- audio.js
+++ audio.js
@@ -1,3 +1,26 @@
-let audio = (function () {
+let audio = (function (Context, Recorder) {
     'use strict';
+    if (!window.AudioContext) throw Error("This module can't be used without AudioContext API.");
+
+    let init = function() {
+        try {
+            Context = new AudioContext();
+            return Promise.resolve(true);
+        } catch (e) {
+            return Promise.reject(Error("Can't initialize audio context"));
+        }
+    }
+
+    let startRecording = function () {
+        return navigator.mediaDevices.getUserMedia({audio: true})
+        .then(function (stream) {
+            Recorder = new MediaRecorder(stream);
+            return Recorder;
+        });
+    }
+
+    return {
+        init,
+        startRecording
+    }
 })();
